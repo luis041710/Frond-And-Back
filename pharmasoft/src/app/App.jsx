ab8ec73475a916db97fc80f5ed64bb7453747ff8
luis <luisdavidguevam20041017@gmail.com>
2026-02-19 20:31:30 -0500

    '..'

---
+++ pharmasoft/src/app/App.jsx
@@ -0,0 +1,28 @@
+import UserForm from "../features/users/components/UserForm"
+import DeleteUseEffect from "../shared/components/DeleteUseEffect"
+import "../styles/global.css"
+import DeleteCounter from "@/shared/components/DeleteCounter"
+
+export default function App() {
+  return(
+    <div className="grid grid-cols-1 justify-items-center">
+      <h1 className="bg-brand text-black text-2x1 font-bold">
+           Pharmasoft
+      </h1>
+    
+    <div>
+       
+      <UserForm/>
+
+    </div>
+
+      <DeleteCounter/>
+
+      <DeleteUseEffect/>
+      
+    </div>
+
+    
+    
+  );
+};
---
+++ pharmasoft/src/features/users/components/UserForm.jsx
@@ -0,0 +1,70 @@
+import Input from "@/shared/components/Input";
+import Button from "@/shared/components/Button";
+
+export default function UserForm(){
+
+    // const handleNameChange = (e) => {
+    //     console.log("Nombre del usuario:", e.target.value)
+    // };
+    const handleEmailBlur = (e) => {
+        console.log("E-mail del usuario:", e.target.value)
+    };
+
+    // Una validación básica
+
+    const handleNameChange = (e) => {
+        console.log("Nombre del usuario:", e.target.value)
+
+        if(e.target.value === ""){
+        console.log("Este campo no puede estar vacío")
+        }
+    };
+    
+
+    return(
+        <div>
+            {/* Formulario para crear el usuario */}
+            
+            <form>
+                
+                <Input
+                label="Nombre"
+                placeholder="Ingrese su nombre"
+                onChange={handleNameChange} //Se da cuando ingreso datos en un imput
+                >
+                </Input>
+
+                <Input
+                label="E-mail"
+                placeholder="Ingrese su E-mail"
+                onBlur={handleEmailBlur} //Se da cuando ingreso datos en un imput
+                >
+                </Input>
+
+                {/* Actions */}
+                <div className="flex items-center justify-center gap-12 py-6">
+                    <Button
+                     variant="secondary"
+                     size="sm"
+                     onClick={() => console.log("Oprimió cancelar")}>
+                        Cancelar
+                    </Button>
+                    <Button
+                     variant="primary"
+                     size="md"
+                     type= "submit"
+                     onSubmit={() => console.log("Oprimió guardar")}>
+                        Guardar
+                    </Button>
+                </div>
+            </form>
+        </div>
+
+
+
+
+
+
+    )
+
+}
